c4fd343a9e4d3c2127fd35c2b97ec076191d695b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-14 04:25:00 +0600

    create unlikeBook functionality in booksController

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -44,4 +44,44 @@ const likeBook = (req, res) => {
   res.status(200).json(AllBooks);
 }
 
-module.exports = { getAllBooks, likeBook };
+const unlikeBook = (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+  let unlikedByUser = [];
+  let index = -1;
+
+  for (let i = 0; i < Books.length; i++) {
+    if (Books[i]["likedBy"] === userId) {
+      unlikedByUser = Books[i]["unlikedBooks"]
+      index = i;
+      break;
+    }
+  }
+
+  const bookId = id;
+
+  for (let i = 0; i < unlikedByUser.length; i++) {
+    if (unlikedByUser[i] === bookId) {
+      return res.status(200).json({ "message": "You have already disliked it" }).send()
+    }
+  }
+
+  let unliked = false;
+
+  for (let i = 0; i < AllBooks.length; i++) {
+    if (AllBooks[i]._id === id) {
+      unliked = true;
+      AllBooks[i].likes = AllBooks[i].likes > 1 ? AllBooks[i].likes - 1 : AllBooks[i].likes;
+      Books[index]["unlikedBooks"].push(bookId);
+    }
+
+  }
+
+  if (!unliked) {
+    return res.status(400).json({ "message": "Book doesn't exist" }).send()
+
+  }
+  res.status(200).json(AllBooks);
+}
+
+module.exports = { getAllBooks, likeBook, unlikeBook };
