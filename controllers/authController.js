b990337b2a5e5382d29811a7663f7a4a369fae9e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-15 16:03:51 +0600

    create authController

---
+++ controllers/authController.js
@@ -0,0 +1,27 @@
+const { verifyEmailAndPassword } = require("../services/authService");
+const { generateAuthToken } = require("../services/tokenService");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (email && password) {
+    let id = verifyEmailAndPassword(email, password)
+    if (id) {
+      try {
+        const token = await generateAuthToken(id);
+        res.status(200).json({ "tokens": token })
+      }
+      catch (err) {
+        res.status(404).json(err)
+      }
+    }
+    else {
+      res.status(404).json({ "message": "User not found" });
+    }
+  }
+  else {
+    res.status(404).json({ "message": "Please enter email and password " });
+  }
+}
+
+module.exports = { login };
