7d1a4a3f65879a82b549ff8b99c07f1d05d3180f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-15 15:59:49 +0600

    create auth middlewares

---
+++ middlewares/auth.js
@@ -0,0 +1,34 @@
+const passport = require("passport");
+
+const verifyCallback = (req, resolve, reject) => async (err, user, info) => {
+  if (info) {
+    reject({ "message": info.message })
+  }
+  if (err) {
+    reject(err)
+  }
+  if (!user) {
+    reject({ "message": "User not found" })
+
+  }
+  if (user) {
+    req.user = user;
+    resolve();
+  }
+};
+
+const auth = async (req, res, next) => {
+  return new Promise((resolve, reject) => {
+    passport.authenticate(
+      "jwt",
+      { session: false },
+      verifyCallback(req, resolve, reject)
+    )(req, res, next);
+  })
+    .then(() => next())
+    .catch((err) => {
+      res.status(401).json(err);
+    });
+};
+
+module.exports = auth;
